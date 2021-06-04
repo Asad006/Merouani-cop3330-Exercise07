f00f76c36d899485d04fa071fd98a31ff82d8916
meroa006 <merouani.ass@gmail.com>
2021-06-04 10:45:11 -0400

    base solution + copyrights

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,29 +1,38 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Asad merouani
+ */
 package org.example;
 
 import java.util.Scanner;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
+public class App {
     public static Scanner scanner = new Scanner(System.in);
 
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
+
         App app = new App();
-        int area =app.getRoomArea(scanner);
+
+        int area = app.getRoomArea(scanner);
+
         displayArea(area);
-        converttToSI(area);
+        convertToSI(area);
     }
 
-    private static void converttToSI(int area) {
-        System.out.printf("%.3f square meters\n",area*0.09290304);
+    private static void convertToSI(int area) {
+        final float conversionFactor = 0.09290304f;
+        float areaSi;
+
+        areaSi = area * conversionFactor;
+
+        System.out.printf("%.3f square meters\n", areaSi);
     }
 
     private static void displayArea(int area) {
-        System.out.printf("The area is\n%d square feet\n",area);
+        System.out.printf("The area is\n%d square feet\n", area);
     }
 
     private int getRoomArea(Scanner scanner) {
@@ -31,14 +40,14 @@ public class App
         int width;
 
         System.out.println("What is the length of the room in feet?");
-        length= Integer.parseInt(scanner.nextLine());
+        length = Integer.parseInt(scanner.nextLine());
 
         System.out.println("What is the length of the room in feet?");
-        width= Integer.parseInt(scanner.nextLine());
+        width = Integer.parseInt(scanner.nextLine());
 
-        System.out.printf("You entered dimensions of %d feet by %d feet.",length,width);
+        System.out.printf("You entered dimensions of %d feet by %d feet.", length, width);
 
-        return width*length;
+        return width * length;
     }
 
 
